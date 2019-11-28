8284b5fb3b52c0606ee98e31283ae8729d0c756d
carterdacat <56525188+carterdacat@users.noreply.github.com>
2019-11-28 12:23:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -95,4 +94,4 @@ client.on('message', message => {
 	}
 }); 
 
-client.login(token);
+client.login(process.env.BOT_TOKEN);
