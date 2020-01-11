749777491785508f5736621ec8332e6be098aa23
carterdacat <56525188+carterdacat@users.noreply.github.com>
2020-01-10 19:26:30 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,1 +1,8 @@
-
+const Discord = require('discord.js');
+module.exports = {
+      name: 'ban',
+      description: 'Bans a user',
+      aliases: ['Ban'],
+      usage: '[user]',
+      execute(message, args) {
+         let mentionedUser = 
