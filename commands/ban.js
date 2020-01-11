5eaa9a43b8000b0ab8292f76909fa41392cd9000
carterdacat <56525188+carterdacat@users.noreply.github.com>
2020-01-10 20:48:11 -0500

    Delete ban.js

--- commands/ban.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-module.exports = {
-      name: 'ban',
-      description: 'Bans a user',
-      aliases: ['Ban'],
-      usage: '[user]',
-      execute(message, args) {
-         let mentionedUser = 
