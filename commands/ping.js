10c376b3a0a848ccc2d4803874bb200c62f6a7a4
Miguel-cyber <54266590+Miguel-cyber@users.noreply.github.com>
2020-01-12 11:59:59 +0000

    Delete ping.js

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js');
-
-client.on('message', message => {
-    if(message.content.startsWith("!ping")) {
-            message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");        
-    }
-}
