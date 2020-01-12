4ad6a9a7eed700d43c8bcd69e9a33b02bbb645b2
carterdacat <56525188+carterdacat@users.noreply.github.com>
2020-01-12 00:52:48 -0500

    mabye?

--- commands/setting.js
+++ commands/setting.js
@@ -5,12 +5,13 @@ module.exports = {
 	description: 'Changes my setting',
 	aliases: ['Settings','settings','Setting'],
 	usage: '[setting] [boolagan]',
-	execute(,message, args) {
-    let roles = message
-    if (roles.includes("Mod" || 'Moderator' || 'Admin')) {
-     let mod = true
-     } else {
-       let mod = false 
-       }
-    console.log(mod) 
-    console.log(roles)
+	execute(client, message, args) {
+    		let roles = message
+    		if (roles.includes("Mod" || 'Moderator' || 'Admin')) {
+     			let mod = true
+     		} else {
+       			let mod = false 
+       		}
+    		console.log(mod) 
+    		console.log(roles)
+	}
