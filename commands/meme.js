72f4df13151ee6ae0934634b861e7b59b8327c03
carterdacat <56525188+carterdacat@users.noreply.github.com>
2020-01-12 03:05:17 -0500

    hotfix 4?

--- commands/meme.js
+++ commands/meme.js
@@ -32,7 +32,7 @@ module.exports = {
             .setTitle(message.author.tag)
             .setAuthor(message.author.tag, message.author.avatarURL
             .addField('The SubReddit was suggested by:', `<@${message.author.tag}>${message.author.tag}`)
-            .addField(`The SubReddit they sugeested is:`, args[1])
+            .addField(`The SubReddit they sugeested is:`, `${args[1]}` )
             channels.get('665825128415887370').send(whoAdded);          
             subReddits.push(`${args[1]}`)
       } else if (args[2]){
