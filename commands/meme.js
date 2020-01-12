d202078c31aff9795ee8b6f73735ad8bba64a7dc
carterdacat <56525188+carterdacat@users.noreply.github.com>
2020-01-12 03:47:30 -0500

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -31,7 +31,7 @@ module.exports = {
                     .setColor('#4DF8E8')
                     .setTitle(message.author.tag)
                     .setAuthor(message.author.tag, message.author.avatarURL)
-                    .addField('The SubReddit was suggested by:', `<@${message.author.tag}>${message.author.tag}`)
+                    .addField('The SubReddit was suggested by:', `<@${message.author.id}>  ${message.author.tag}`)
                     .addField('The SubReddit they sugeested is:', args[1])
                     client.channels.get('665825128415887370').send(whoAdded); 
                     subReddits.push(`${args[1]}`)
