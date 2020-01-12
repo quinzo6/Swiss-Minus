0d9f3a222953c97dd19a9d89a336edb24d1ce9b1
carterdacat <56525188+carterdacat@users.noreply.github.com>
2020-01-11 21:31:36 -0500

    Update setting.js
    First test of making sure its a mod

--- commands/setting.js
+++ commands/setting.js
@@ -1,1 +1,18 @@
-
+const fs = require('fs')
+const Discord = require('discord.js');
+module.exports = {
+	name: 'setting',
+	description: 'Changes my setting',
+	aliases: ['Settings'],
+	usage: '[setting] [boolagan]',
+	execute(message, args) {
+   let messageSenderRoles = message.member.roles.map(r => r)
+   if (messageSenderRoles.includes("Mod" || 'Moderator' || 'Admin')) {
+     let mod = true
+     } else {
+       let mod = false 
+       }
+    console.log(mod) 
+    console.log(messageSenderRoles)
+  }
+   
