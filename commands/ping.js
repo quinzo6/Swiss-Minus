df0fa229fb53f850de49606cace606f5b825ea43
Miguel-cyber <54266590+Miguel-cyber@users.noreply.github.com>
2020-01-15 07:52:05 +0100

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+module.exports = {
+      name: 'ping',
+      description: 'Gets ping',
+      aliases: ['Ping'],
+      cooldown: 5,
+      execute(client, message, args) {
+              const m = await message.channel.send("Ping?");
+              message.delete()
+              .then(message.channel.send(new Discord.RichEmbed().setAuthor(message.author.tag, message.author.avatarURL).setTitle('Pong!').setColor('#4DF8E8').addField(':timer:Ping:',`${m.createdTimestamp - message.createdTimestamp}ms.))
+              .catch(message.chanel.send(new Discord.RichEmbed().setAuthor(message.author.tag, message.author.avatarURL).setTitle('Error!').setColor('#E80C0C').addField('Error:', error)
+}
