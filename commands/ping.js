c9e0e0a565c2fee891cddb668e4e049a9d14a1c2
Miguel-cyber <54266590+Miguel-cyber@users.noreply.github.com>
2020-01-15 15:54:28 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -7,6 +7,6 @@ module.exports = {
      execute: async(client, message, args) {
               const m = await message.channel.send("Ping?");
               message.delete()
-              .then(message.channel.send(new Discord.RichEmbed().setAuthor(message.author.tag, message.author.avatarURL).setTitle('Pong!').setColor('#4DF8E8').addField(':timer:Ping:',`${m.createdTimestamp - message.createdTimestamp}ms.))
+              .then(message.channel.send(new Discord.RichEmbed().setAuthor(message.author.tag, message.author.avatarURL).setTitle('Pong!').setColor('#4DF8E8').addField(':timer: Ping:',`${m.createdTimestamp - message.createdTimestamp}`))
               .catch(message.chanel.send(new Discord.RichEmbed().setAuthor(message.author.tag, message.author.avatarURL).setTitle('Error!').setColor('#E80C0C').addField('Error:', error)
 }
