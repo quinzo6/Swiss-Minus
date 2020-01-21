b46f89bc52dc2b08e292cf4e03372a88d5300c3d
neoney <30625554+n3oney@users.noreply.github.com>
2020-01-21 11:44:52 +0100

    simplify say.js

--- commands/say.js
+++ commands/say.js
@@ -2,58 +2,48 @@
 /* eslint-disable no-var */
 /* eslint-disable vars-on-top */
 const Discord = require('discord.js');
-const users = require('./sayusers.json');
+const userids = require('./sayusers.json');
 
 module.exports = {
   name: 'say',
   description: 'Says a message!',
   usage: '[channel] [message]',
   execute(client, message, args) {
-    const userids = users.usersID;
-    const messageCount = args.length;
     const mentionedChannel = message.mentions.channels.first() || client.channels.get(args[0]);
-    if (userids.includes(message.author.id)) {
-      var mod = true;
-    } else {
-      mod = false;
-    }
-    if (mod === false) {
-      const noPerms = new Discord.RichEmbed();
-      noPerms
+    const mod = userids.includes(message.author.id);
+    if (!mod) {
+      const embed = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL)
         .setTitle('Missing Permisions')
         .setColor('#F90B0B')
-        .addField('Missing Perms!', `Hey <@${message.author.id}>, you are missing permissions to use this command.`);
-      return message.channel.send(noPerms);
+        .addField('Missing Perms!', `Hey ${message.author}, you are missing permissions to use this command.`);
+      return message.channel.send(embed);
     }
     if (!mentionedChannel) {
-      const none = new Discord.RichEmbed();
-      none
+      const embed = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL)
         .setTitle('Invalid Channel!')
         .setColor('F90B0B')
-        .addField('Whats that?', 'Thats not a channel!');
-      return message.channel.send(none);
+        .addField('Whats that?', 'That\'s not a channel!');
+      return message.channel.send(embed);
     }
     if (mentionedChannel && !args[1]) {
-      const oof = new Discord.RichEmbed();
-      oof
+      const embed = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL)
         .setTitle('Error!')
         .setColor('#F90B0B')
-        .addField('I think you forgot something', 'Your frogot what you wanted to say! Think deeper');
-      return message.channel.send(oof);
+        .addField('I think you forgot something', 'Your forgot what you wanted to say! Think deeper');
+      return message.channel.send(embed);
     }
-    const messages1 = args.slice(1, messageCount).join(' ');
-    const log = new Discord.RichEmbed();
-    log
+    const messages1 = args.slice(1).join(' ');
+    const embed = new Discord.RichEmbed()
       .setAuthor(message.author.tag, message.author.avatarURL)
       .setTitle('Say Cmd Log')
       .setColor('#F5AA42')
       .addField('This person used the command!:', `<@${message.author.id}>`)
       .addField('The message was:', messages1);
     const chl = client.channels.get('668987003517534259');
-    chl.send(log);
+    chl.send(embed);
     return mentionedChannel.send(messages1);
   },
 };
