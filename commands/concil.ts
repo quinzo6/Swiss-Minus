de80eed47810b75190ea207ece07d779ac3f68e7
Carter Nabors <carter@Carters-MacBook-Pro.local>
2020-02-12 09:02:25 -0500

    2.5.3 Changes the image for council

--- commands/concil.ts
+++ commands/concil.ts
@@ -24,11 +24,11 @@ export let description = 'The council will decide your fate';
 export async function execute(client: Client, message: Message, args: string[], db: PgClient) {
     let mentioned = message.mentions.members.first() || message.guild.members.get(args[0]);
     if (!mentioned) {
-        return message.channel.send(new Attachment('https://media.discordapp.net/attachments/592463507124125706/676981051179925527/image0.png?width=1058&height=582', 'council.jpg'))
+        return message.channel.send(new Attachment('https://media.discordapp.net/attachments/664235191065247775/677146760807710730/image0.png?width=1069&height=582', 'council.jpg'))
     }
     message.delete()
     message.channel.send(`<@${mentioned.id}>, Your time has come...`)
     setTimeout(function () {
-        return message.channel.send(new Attachment('https://media.discordapp.net/attachments/592463507124125706/676981051179925527/image0.png?width=1058&height=582', 'council.jpg'))
+        return message.channel.send(new Attachment('https://media.discordapp.net/attachments/664235191065247775/677146760807710730/image0.png?width=1069&height=582', 'council.jpg'))
     }, 2000)
 }
