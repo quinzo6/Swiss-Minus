e083aadd2a77725391e30b7daa0135978b66cb55
Carter Nabors <carter@Carters-MacBook-Pro.local>
2020-02-13 21:37:12 -0500

    2.6.3

--- commands/concil.ts
+++ commands/concil.ts
@@ -24,11 +24,11 @@ export let description = 'The council will decide your fate';
 export async function execute(client: Client, message: Message, args: string[], db: PgClient) {
     let mentioned = message.mentions.members.first() || message.guild.members.get(args[0]);
     if (!mentioned) {
-        return message.channel.send(new Attachment('https://media.discordapp.net/attachments/660242184192131138/677561873012621312/unknown.png', 'council.jpg'))
+        return message.channel.send(new Attachment('https://media.discordapp.net/attachments/592463507124125706/677702569908371498/council.jpg', 'council.jpg'))
     }
     message.delete()
     message.channel.send(`<@${mentioned.id}>, Your time has come...`)
     setTimeout(function () {
-        return message.channel.send(new Attachment('https://media.discordapp.net/attachments/660242184192131138/677561873012621312/unknown.png', 'council.jpg'))
+        return message.channel.send(new Attachment('https://media.discordapp.net/attachments/592463507124125706/677702569908371498/council.jpg', 'council.jpg'))
     }, 2000)
 }
