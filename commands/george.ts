f2e0433f59817653cd43175cf65b34bcc1801d36
Carter Nabors <carter@Carters-MacBook-Pro.local>
2020-02-16 11:06:53 -0500

    george

---
+++ commands/george.ts
@@ -0,0 +1,10 @@
+import Discord, {
+    Attachment, Client, Message
+} from 'discord.js'
+
+export let name = 'george'
+
+
+export async function execute(client: Client, message: Message, args: string[]) {
+    return message.channel.send(new Attachment('https://cdn.discordapp.com/attachments/592463507124125706/678632428453101568/20200215_202029.png', 'george.jpg'))
+}
