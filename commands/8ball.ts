c6edfbc5e7657305dc9fd602a5d9b8028907f704
DevShaded <devshaded@gmail.com>
2020-03-08 16:31:57 +0100

    edit: cleaned code and updated choices array

--- commands/8ball.ts
+++ commands/8ball.ts
@@ -19,21 +19,37 @@ export let cooldown = 5;
 export let aliases = ['8ball', 'eightball', 'future'];
 
 export async function execute(client: Client, message: Message, _args: string[]) {
-        const choices = [
-            "Outlook Good!",
-            "Outlook not so good.",
-            "My Reply is : YES",
-            "My Sources tell me no",
-            "My reply is No",
-            "You may rely on it."
-        ];
-        const randomOption = choices[Math.floor(Math.random() * choices.length)];
-        const embed = new Discord.MessageEmbed()
-            .setAuthor(message.author.tag, message.author.avatarURL())
-                .setTitle('8Ball')
-                .setColor(swiss_blue)
-                .addField('Fortune:', randomOption)
-                .setFooter(version)
-                .setTimestamp() 
-                message.channel.send(embed);
-            }
+    
+    const choices = [
+        'It is certain',
+        'It is decidedly so',
+        'Without a doubt',
+        'Yes - definitely',
+        'You may rely on it',
+        'As I see it, yes',
+        'Most likely',
+        'Outlook good',
+        'Yes',
+        'Signs point to yes',
+        'Reply hazy, try again',
+        'Ask again later',
+        'Better not tell you now',
+        'Cannot predict now',
+        'Concentrate and ask again',
+        'Dont count on it',
+        'My reply is no',
+        'My sources say no',
+        'Outlook not so good',
+        'Very doubtful'
+    ];
+
+    const randomOption = choices[Math.floor(Math.random() * choices.length)];
+    const embed = new Discord.MessageEmbed()
+        .setAuthor(message.author.tag, message.author.avatarURL())
+        .setTitle('8Ball')
+        .setColor(swiss_blue)
+        .addField('Fortune:', randomOption)
+        .setFooter(version)
+        .setTimestamp() 
+    message.channel.send(embed);
+}
