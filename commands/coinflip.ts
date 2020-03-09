90eb072a16499482958278585d0e9b44cb938f7e
DevShaded <devshaded@gmail.com>
2020-03-09 20:14:00 +0100

    edit: cleaned code from line 3-5

--- commands/coinflip.ts
+++ commands/coinflip.ts
@@ -1,17 +1,8 @@
-import Discord, {
-    Client,
-    Message,
-    MessageEmbed
-} from "discord.js";
-import {
-    swiss_blue
-} from "../config";
-import {
-    error_red
-} from "../config"
-import {
-    version
-} from '../package.json'
+import Discord, { Client, Message, MessageEmbed } from "discord.js";
+
+import { swiss_blue } from "../config";
+import { error_red } from "../config"
+import { version } from '../package.json'
 
 export let name = "coinflip";
 export let description = "Game: Coin, return head or tails to the user";
