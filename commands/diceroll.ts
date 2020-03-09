e11be8f66d5a09beaef35932a7e59871032c2352
DevShaded <devshaded@gmail.com>
2020-03-09 21:24:27 +0100

    add: Dice roll, game

---
+++ commands/diceroll.ts
@@ -0,0 +1,34 @@
+import Discord, { Client, Message, MessageEmbed } from "discord.js";
+
+import { swiss_blue } from "../config";
+import { error_red } from "../config"
+import { version } from '../package.json'
+
+export let name = "diceroll";
+export let description = "Game: Dice roll, returns a number between 1 and 6";
+export let cooldown = 5;
+export let aliases = ['diceroll', 'dice'];
+
+export async function execute(client: Client, message: Message, _args: string[]) {
+
+    const diceArray = [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6'
+    ];
+
+    const answer = diceArray[Math.floor(Math.random() * diceArray.length)];
+
+    const embed = new Discord.MessageEmbed()
+        .setAuthor(message.author.tag, message.author.avatarURL())
+        .setTitle('Dice roll')
+        .setColor(swiss_blue)
+        .addField('I threw a dice and it turned it to be', answer)
+        .setFooter(version)
+        .setTimestamp() 
+    message.channel.send(embed);
+
+}
