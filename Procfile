1b35add6c6906058f1e3a064e4b1ac7a19d75766
Carter Nabors <carter@Carters-MacBook-Pro.local>
2020-03-10 07:24:05 -0400

    Moves the site to a whole new place

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-worker: node index.js
-web: npm run wstart
+worker: node index.ts
--- src/pages/products/product-1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Menu from 'components/Menu';
-
-import './product-1.css';
-import productPicture from './download.png';
-
-ReactDOM.render(<Menu />, document.getElementById('menu'));
-document.getElementById('product-pic').setAttribute('src', productPicture);
