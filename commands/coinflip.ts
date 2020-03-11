5185209a2e5624857f0fa8a74f375c2868e54923
carterdacat <56525188+carterdacat@users.noreply.github.com>
2020-03-11 07:38:37 -0400

    Update coinflip.ts

--- commands/coinflip.ts
+++ commands/coinflip.ts
@@ -7,7 +7,7 @@ import { version } from '../package.json'
 export let name = "coinflip";
 export let description = "Game: Coin, return head or tails to the user";
 export let cooldown = 5;
-export let aliases = ['coinflip', 'coin'];
+export let aliases = ['coinflip', 'coin', 'roll'];
 
 export async function execute(client: Client, message: Message, _args: string[]) {
 
