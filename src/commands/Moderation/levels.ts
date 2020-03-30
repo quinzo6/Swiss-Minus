4a7e1b23619d28dc24eb6a873f05424ef232a4ce
Doron Segev <30154552+nerdoron@users.noreply.github.com>
2020-03-30 10:51:17 +0300

    fixed typos

--- src/commands/Moderation/levels.ts
+++ src/commands/Moderation/levels.ts
@@ -12,16 +12,16 @@ export async function execute(
     args: string[]
   ) {
 
-    message.channel.send("**Level Roles :arrow_double_up::\n"
-     + "These roles are achiveable by gaining random exp points by chatting. Not every message will give you exp so spamming won't help\n" + 
-     "Member: Level 0\n" + 
-     "Active Member: Level 5\n" +
-     "SwissClub: Level 10\n" +
-     "SwissGold: Level 15\n " + 
-     "SwissGold+: Level 20\n" + 
-     "SwissPlatinum: Level 25\n" + 
-     "SwissPlatinum+: Level 35\n" + 
-     "SwissElite: Level 45. \n" + 
+    message.channel.send("**Level Roles :arrow_double_up::\n**"
+     + "These roles are achiveable by gaining random exp points by chatting. Not every message will give you exp so spamming won't help.\n" + 
+     "Member: Level 0,\n" + 
+     "Active Member: Level 5,\n" +
+     "SwissClub: Level 10,\n" +
+     "SwissGold: Level 15,\n " + 
+     "SwissGold+: Level 20,\n" + 
+     "SwissPlatinum: Level 25,\n" + 
+     "SwissPlatinum+: Level 35,\n" + 
+     "SwissElite: Level 45.\n" + 
      "You can view your level by doing `=r` in <#592770427948892171>");
 
 }
